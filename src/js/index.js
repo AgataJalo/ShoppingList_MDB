efd777664d69f14f7a964b9d754f9dfd7ef86d95
Agata <agatajalowska@gmail.com>
2020-07-17 21:45:53 +0200

    Render

--- src/js/index.js
+++ src/js/index.js
@@ -17,11 +17,36 @@ const state = [
   new CategoryBucket('owoce', [
     new Product('jabłko', 2, inputNumTypeWeight),
     new Product('arbuz', 1, inputNumTypePieces),
+    new Product('banan', 6, inputNumTypePieces),
   ]),
 ];
 
 function render() {
   console.log(state);
+  addToList();
 }
 
 render();
+
+function addToList() {
+  const shoppingList = document.createElement('ul');
+  const shoppingBoard = document.getElementById('shoppingBoard');
+  shoppingBoard.appendChild(shoppingList);
+  // Loop through CategoryBucket array
+  for (let i = 0; i < state.length; i++) {
+    const categoryBucket = state[i];
+    const categoryBucketElement = document.createElement('li');
+    shoppingList.appendChild(categoryBucketElement);
+    categoryBucketElement.appendChild(document.createTextNode(categoryBucket.name));
+    const categoryNameList = document.createElement('ul');
+    categoryBucketElement.appendChild(categoryNameList);
+    // Loop through Product array
+    for (let j = 0; j < categoryBucket.products.length; j++) {
+      const aaaproduct = categoryBucket.products[j];
+
+      const productElement = document.createElement('li');
+      productElement.appendChild(document.createTextNode(aaaproduct.name));
+      categoryNameList.appendChild(productElement);
+    }
+  }
+}
