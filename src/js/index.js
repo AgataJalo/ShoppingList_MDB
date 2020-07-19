eb5f14aefe05ff033531280ac23ac29a2d079cd3
Agata <agatajalowska@gmail.com>
2020-07-19 15:48:58 +0200

    Format code

--- src/js/index.js
+++ src/js/index.js
@@ -6,21 +6,20 @@ export default {
   mdb,
 };
 
-
 const inputNumTypePieces = 0;
 const inputNumTypeWeight = 1;
 
 const state = [
   new CategoryBucket('pieczywo', [
-  new Product('chleb', 2, inputNumTypePieces),
-  new Product('bułka', 1, inputNumTypePieces),
+    new Product('chleb', 2, inputNumTypePieces),
+    new Product('bułka', 1, inputNumTypePieces),
   ]),
-   new CategoryBucket('owoce', [
-     new Product('jabłko', 2, inputNumTypeWeight),
-     new Product('arbuz', 1, inputNumTypePieces),
-     new Product('banan', 6, inputNumTypePieces),
-     new Product('banan', 1, inputNumTypeWeight),
-   ]),
+  new CategoryBucket('owoce', [
+    new Product('jabłko', 2, inputNumTypeWeight),
+    new Product('arbuz', 1, inputNumTypePieces),
+    new Product('banan', 6, inputNumTypePieces),
+    new Product('banan', 1, inputNumTypeWeight),
+  ]),
 ];
 
 function render() {
@@ -30,21 +29,19 @@ function render() {
   shoppingBoard.innerHTML = '';
 
   const shoppingList = document.createElement('table');
-  shoppingList.classList.add("table");
+  shoppingList.classList.add('table');
 
   const tbody = document.createElement('tbody');
   shoppingList.appendChild(tbody);
 
-
-  
   shoppingBoard.appendChild(shoppingList);
   // Loop through CategoryBucket array
-  let counter=0;
+  let counter = 0;
 
   for (let i = 0; i < state.length; i++) {
     const categoryBucket = state[i];
-    if(categoryBucket.products.length === 0){
-      continue
+    if (categoryBucket.products.length === 0) {
+      continue;
     }
 
     const categoryTr = document.createElement('tr');
@@ -52,11 +49,10 @@ function render() {
 
     const categoryTd = document.createElement('td');
     categoryTd.classList.add('text-center');
-    categoryTd.setAttribute('colspan','4');
+    categoryTd.setAttribute('colspan', '4');
     categoryTd.textContent = categoryBucket.name;
     categoryTr.appendChild(categoryTd);
     tbody.appendChild(categoryTr);
-
 
     // Loop through Product array
     for (let j = 0; j < categoryBucket.products.length; j++) {
@@ -67,49 +63,49 @@ function render() {
       const tdName = document.createElement('td');
       tdName.textContent = product.name;
       productTr.appendChild(tdName);
-      
+
       const tdNum = document.createElement('td');
       tdNum.textContent = product.inputNum;
       productTr.appendChild(tdNum);
-      
+
       const tdNumType = document.createElement('td');
-      if(product.inputNumType === inputNumTypeWeight){
-        if(product.inputNum === 1){
-          tdNumType.textContent = "gram";
+      if (product.inputNumType === inputNumTypeWeight) {
+        if (product.inputNum === 1) {
+          tdNumType.textContent = 'gram';
         } else {
-          tdNumType.textContent = "grams";
+          tdNumType.textContent = 'grams';
         }
-       } else {
-         if(product.inputNum === 1){
-           tdNumType.textContent = "piece";
-         } else {
-           tdNumType.textContent = "pieces";
-         }
+      } else {
+        if (product.inputNum === 1) {
+          tdNumType.textContent = 'piece';
+        } else {
+          tdNumType.textContent = 'pieces';
+        }
       }
-      
+
       productTr.appendChild(tdNumType);
 
       const tdRemove = document.createElement('td');
-      tdRemove.className ="d-flex justify-content-end"
+      tdRemove.className = 'd-flex justify-content-end';
       const removeBtn = document.createElement('button');
-      removeBtn.className = "btn"
-      removeBtn.textContent = "delete"
+      removeBtn.className = 'btn';
+      removeBtn.textContent = 'delete';
       tdRemove.appendChild(removeBtn);
-      
-      productTr.appendChild(tdRemove);
-      
-      tbody.appendChild(productTr);
-      
-      let removeFunction = function () {
-        categoryBucket.products.splice(j,1);
-        render();
-       }
 
-       removeBtn.onclick = removeFunction;
+      productTr.appendChild(tdRemove);
+
+      tbody.appendChild(productTr);
+
+      let removeFunction = function () {
+        categoryBucket.products.splice(j, 1);
+        render();
+      };
+
+      removeBtn.onclick = removeFunction;
     }
   }
   let counterDiv = document.getElementById('counterDiv');
-  counterDiv.textContent = "Products on list: "+counter;
+  counterDiv.textContent = 'Products on list: ' + counter;
 }
 
 render();
@@ -120,47 +116,46 @@ addButton.addEventListener('click', addToList);
 function addToList() {
   const productNameInput = document.getElementById('productNameInput');
   const inputValue = productNameInput.value;
-  if(inputValue == 0){
+  if (inputValue == 0) {
     return;
   }
   const categorySelect = document.getElementById('categorySelect');
   let selectedCategory = categorySelect.options[categorySelect.selectedIndex].value;
-  
 
-  if (selectedCategory===""){
+  if (selectedCategory === '') {
     return;
   }
 
   let categoryBucket;
-  for (let i=0; i<state.length; i++){
-    if(state[i].name === selectedCategory){
-      categoryBucket=state[i];
+  for (let i = 0; i < state.length; i++) {
+    if (state[i].name === selectedCategory) {
+      categoryBucket = state[i];
       break;
     }
   }
-  
-  if (categoryBucket === undefined){
-    categoryBucket = new CategoryBucket (selectedCategory, [])
+
+  if (categoryBucket === undefined) {
+    categoryBucket = new CategoryBucket(selectedCategory, []);
     state.push(categoryBucket);
   }
 
   //num input
   const quantityInput = document.getElementById('quantityInput');
   const quantityInputValue = parseInt(quantityInput.value);
- 
-  if (isNaN(quantityInputValue)){
-         return;
-    }
- 
+
+  if (isNaN(quantityInputValue)) {
+    return;
+  }
+
   //Radios
   const changeRadioQuantity = document.getElementById('changeRadioQuantity');
-  
+
   let quantityTypeValue;
-  if(changeRadioQuantity.checked === true){
+  if (changeRadioQuantity.checked === true) {
     quantityTypeValue = inputNumTypePieces;
   } else {
-    quantityTypeValue=inputNumTypeWeight;
+    quantityTypeValue = inputNumTypeWeight;
   }
-  categoryBucket.products.push(new Product (inputValue, quantityInputValue, quantityTypeValue));
+  categoryBucket.products.push(new Product(inputValue, quantityInputValue, quantityTypeValue));
   render();
 }
